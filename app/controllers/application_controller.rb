d12b48cb9c2116d7694badb01938b934e12b1af2
togra1997 <ryoota1997@gmail.com>
2021-03-16 01:09:19 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def hello
+      render html: "¡Hola, mundo!"
+    end
+
+    def goodby
+        render html:"gooby,world"
+    end
+
+
+end
